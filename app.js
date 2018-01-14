050e2457dc687ae15423e1ec5de8d14d63800876
Gabriel Bolzi <gabriel03899@hotmail.com>
2018-01-14 01:12:12 -0200

    Updated

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var conv = require('./routes/conv')
+var users = require('./routes/users');
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/conv', conv)
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,7 +42,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-app.listen(5000 || ENV['PORT'] || process.env.PORT)
 
 module.exports = app;
